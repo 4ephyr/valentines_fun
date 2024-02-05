7884deed792a1f32b69acbcb2fb78980c274fd02
Mohammed Sahal <technicalsahal25@gmail.com>
2024-02-05 21:29:13 +0530

    Updated

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -18,10 +18,10 @@ function Popup({ onClose, yesSure, content }) {
   }
 
   return (
-    <div className={`bg-gray-400 rounded-md shadow-xl antialiased ${isShaking ? 'animate-shake' : ''} absolute top-0 phone:top-6 md:-top-3 bg-opacity-50 border border-primary flex items-center justify-center`}>
+    <div className={`bg-gray-400 rounded-md shadow-xl antialiased ${isShaking ? 'animate-shake' : ''} absolute top-0 phone:top-3 md:-top-3 bg-opacity-50 border border-primary flex items-center justify-center max-w-[80%]`}>
       <div className="bg-primary p-8 rounded-md">
-        <h2 className="text-2xl font-bold mb-4 text-black z-50">{cStatus ? <span>Me Committed Aayloo</span> : content}</h2>
-        <h2 className="text-2xl font-bold mb-4 text-black z-50">{content === 'Thank God !' ? <span>True love wait cheyyerkum lleh.</span> : ''}</h2>
+        <h2 className="text-2xl font-bold mb-4 text-black z-50">{cStatus ? <span>Me Committed Aayloo &#128513;</span> : content}</h2>
+        <h2 className="text-2xl font-bold mb-4 text-black z-50">{content === 'Thank God !' ? <span className='max-w-[10%]'>True love wait cheyyerkum lleh.</span> : ''}</h2>
         <div className="max-w-fit mx-auto flex gap-4">
           {content === 'You Sure ??' ? (
             <button className="bg-blue-500 text-primary px-4 py-2 rounded-md hover:bg-blue-700" onClick={() => {
